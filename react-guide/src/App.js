1f612baa72d1b5e7fa9f67afc5dfad8c241ed076
MariuszSzatkowski <mariusz.szatkowski@airhelp.com>
2018-11-30 15:06:46 +0100

    zmiany zmiany

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Siemanko</h1>
       </div>
     );
+    // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'siemano') );
   }
 }
 
 export default App;
+
